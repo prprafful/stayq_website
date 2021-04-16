786ba4131e3493a441c8003366dcddafd9ed8c80
Prafful Kumar <kumarprafful54@gmail.com>
2021-04-16 12:19:20 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,8 @@ module.exports = {
     },
     target: 'serverless',
     images: {
-        domains: ['wallpapercave.com', 'stayqrious.com'],
+        domains: ['stayqrious.com'],
+        deviceSizes: [640, 750, 828, 1080, 1200],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     }
 }
